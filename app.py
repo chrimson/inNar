0dadb613c9eff875d4f0ff3080fcd51d1ef2a278
Chris Limson <chrimson@gmail.com>
2026-06-22 19:28:11 +0000

    Allow more than one book

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 auth = HTTPBasicAuth()
 
-STORY_FILE = 'story.txt'
 AUTH_FILE = 'auth.json'
+
 
 @auth.verify_password
 def verify(username, password):
@@ -17,10 +17,11 @@ def verify(username, password):
     if username in auth and auth[username] == password:
         return username
 
-def load_story():
+
+def load_story(story_file):
     story = {}
     scene = ''
-    with open(STORY_FILE, 'r') as f:
+    with open(story_file, 'r') as f:
         lines = f.read().splitlines()
     story['title'] = lines[0]
     story['byline'] = lines[1]
@@ -44,37 +45,43 @@ def load_story():
     first = list(story)[2]
     return story, first
 
-def save_story(data):
-    with open(STORY_FILE, 'w', newline='\n') as f:
+
+def save_story(story_file, data):
+    with open(story_file, 'w', newline='\n') as f:
         clean_data = data.replace('\r', '')
         f.write(clean_data)
 
-STORY, FIRST = load_story()
 
-@app.route('/', methods=['GET', 'POST'])
-def game():
-    node = FIRST
+@app.route('/<story_file>', methods=['GET', 'POST'])
+def game(story_file):
+    story, first = load_story(story_file)
+
+    node = first
     if request.method == 'POST':
         node = request.form.get('node')
 
-    scene = STORY.get(node)
-    return render_template('index.html', title=STORY['title'], byline=STORY['byline'], scene=scene)
+    scene = story.get(node)
+    return render_template('index.html',
+                           title=story['title'],
+                           byline=story['byline'],
+                           story_file=story_file,
+                           scene=scene)
 
-@app.route('/edit', methods=['GET', 'POST'])
+
+@app.route('/<story_file>/edit', methods=['GET', 'POST'])
 @auth.login_required
-def edit_story():
-    global STORY, FIRST
+def edit_story(story_file):
     if request.method == 'POST':
         story_text = request.form.get('story_text')
-        save_story(story_text)
-        STORY, FIRST = load_story()
+        save_story(story_file, story_text)
+        story, first = load_story(story_file)
 
-        return redirect(url_for('game'))
+        return redirect(url_for('game', story_file=story_file))
 
-    with open(STORY_FILE, 'r') as f:
+    with open(story_file, 'r') as f:
         content = f.read()
 
-    return render_template('edit.html', content=content)
+    return render_template('edit.html', story_file=story_file, content=content)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
